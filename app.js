ad71abbdb294b574ce97619786ef15c3d0100c02
salomeEsteves <r.salome.esteves@gmail.com>
2023-10-17 10:58:27 +0100

    Removed ºC to ºF.
    It's working again. Starting from scratch.

--- app.js
+++ app.js
@@ -37,25 +37,7 @@ function changeTemp(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
 }
 
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-  let temperatureElement = document.querySelector("#temperature");
-  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
 
-function displayCelsiusTemperature(event) {
-  event.preventDefault();
-  fahrenheitLink.classList.remove("active");
-  celsiusLink.classList.add("active");
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-}
-
-celsiusTemperature = response.data.main.temp;
-let celsiusTemperature = null;
 
 
 
