a704331783e68fdb7fb27f987692460a3b57db52
salomeEsteves <r.salome.esteves@gmail.com>
2023-10-30 11:23:30 +0000

    Added forecast HTML, CSS and JS

--- app.js
+++ app.js
@@ -25,13 +25,61 @@ function formatDate(date) {
 }
 formatDate(currentTime);
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
+      </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "535cacbb3f8a0df0aeb4790235b9541f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function changeTemp(response) {
   let currentTemperature = document.querySelector("#current-temperature");
   let temperature = Math.round(response.data.main.temp);
   currentTemperature.innerHTML = `${temperature}ºC`;
-
-  let fahrenheitTemperature = document.querySelector("#current-temperature-farenheit");
-  fahrenheitTemperature.innerHTML = `${Math.round((temperature * 9) / 5 + 32)}ºF`;
 
   let h2 = document.querySelector("h2");
   h2.innerHTML = response.data.weather[0].description;
@@ -45,6 +93,8 @@ function changeTemp(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 
